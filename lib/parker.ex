0f7249330a6a16432fbfaf821c2adb59bb663623
ngw <nicholas@fitzdares.com>
2017-08-04 10:06:34 +0100

    Project structure

---
+++ lib/parker.ex
@@ -0,0 +1,18 @@
+defmodule Parker do
+  @moduledoc """
+  Documentation for Parker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Parker.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parker_test.exs
@@ -0,0 +1,8 @@
+defmodule ParkerTest do
+  use ExUnit.Case
+  doctest Parker
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
